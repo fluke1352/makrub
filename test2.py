8210180c103ad2115e9d5471b11a6380803643c2
fluke1352 <56115104+fluke1352@users.noreply.github.com>
2019-10-16 00:10:21 +0700

    Add files via upload

--- test2.py
+++ test2.py
@@ -3,7 +3,6 @@ faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 def draw_face(img, classifier, scaleFactor, minNeighbors, color, text):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     features = classifier.detectMultiScale(gray, scaleFactor, minNeighbors)
-    coords = []
     for (x, y, w, h) in features:
         cv2.rectangle(img, (x, y), (x+w, y+h), color, 3)
         cv2.putText(img, text, (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1)
