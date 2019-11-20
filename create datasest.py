c4c3e8bc949bcf17da4f55c0b90dba4ce3a32b1b
woot2001x <56569795+woot2001x@users.noreply.github.com>
2019-11-20 15:08:04 +0700

    for create data picture of student

--- create datasest.py
+++ create datasest.py
@@ -17,17 +17,22 @@ def draw_object(img, classifier, scaleFactor, minNeighbors, color):
 def detect(img, faceCascade, img_id):
     img, position = draw_object(img, faceCascade, 1.1, 10, (0, 0, 0))
     if len(position) == 4:
-        id = 62070061 # XXX = studenID you can use for loop auto run studenID
+        id = 62070171 # XXX = studenID you can use for loop auto run studenID
         cut_face = img[position[1]:position[1]+position[3], position[0]:position[0]+position[2]]
         dataset(cut_face, id, img_id)
     return img
 
 img_id = 0
-cap = cv2.VideoCapture("62070061.jpg")#XXX = studenID you can use for loop auto run studenID
+cap = cv2.VideoCapture(0)#XXX = studenID you can use for loop auto run studenID
 
-# while True:
-ret, frame = cap.read()
-frame = detect(frame, faceCascade, img_id)
+while True:
+    ret, frame = cap.read()
+    frame = detect(frame, faceCascade, img_id)
+    cv2.imshow('frame', frame)
+    img_id += 1
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        create(member)
+        break
 
 cap.release()
 cv2.destroyAllWindows()
